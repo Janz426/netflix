1c766aec0f744b8398eccca37092bc5110a9abc2
unknown <jhonas_janz@hotmail.com>
2024-03-25 09:07:37 -0300

    integração de novos componentes como banner

--- src/components/Row.js
+++ src/components/Row.js
@@ -5,7 +5,7 @@ import './Row.css'
 
 export default function Row({title,path,isLarge}) {
     const [filmes, SetFilmes] = useState()
-    let image_host = 'https://image.tmdb.org/t/p/original/'
+    let imageHost = 'https://image.tmdb.org/t/p/original/'
     
     let fetchData =async()=> {
 
@@ -25,20 +25,24 @@ export default function Row({title,path,isLarge}) {
 
     },[])
 
-    return (
-        <div className='teste'>            
-            {title}
+    return (        
+        <div className='row-container'>
+                        
+            <h2 className='row-header'>{title}</h2>
 
-            <div className='imagens'>
+            <div className='row-cards'>
                 {filmes?.map( (filme) => {
-                    let cl = isLarge?'large':'normal'
                     
-                    return <img className={cl} key={filme?.id} src={image_host + filme?.backdrop_path}></img>
+                    return(
+                        <img
+                        className={`movie-card ${isLarge && "movie-card-large"}` }
+                        key={filme.id} 
+                        src={imageHost + (isLarge?filme.backdrop_path:filme.poster_path)} 
+                        alt={filme.name} >
+                        </img>
+                    )
                 })}
-                
             </div>
-
-
         </div>
     )
 }
