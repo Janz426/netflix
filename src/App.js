3f02b65d7aed9d3f352003f92e53f28643f2af84
unknown <jhonas_janz@hotmail.com>
2024-03-25 08:30:43 -0300

    nova versao

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import './App.css';
-import { categories } from '../api';
+import { categories } from './api';
 import Row from './components/Row'
 
 function App() {
   return (
     <div>
 
+        {/*Navbar*/}
+        {/*Banner*/}
+        {/*Categorias - Linhas*/}
+
+
         {categories.map( (category) => {
 
             return  <Row 
                         key = {category.name}
-                        title = {category.name}
-                        isLarge = {category.name}
+                        title = {category.title}
+                        isLarge = {category.isLarge}
+                        path = {category.path}
                        
              />
 
--- src/components/Row.js
+++ src/components/Row.js
@@ -1,10 +1,13 @@
 import { useEffect } from 'react'
-import './Row.css'
+import {useState} from 'react';
 import {getData} from '../api'
+import './Row.css'
 
 export default function Row({title,path,isLarge}) {
-
-    let fetchData =async()=>{
+    const [filmes, SetFilmes] = useState()
+    let image_host = 'https://image.tmdb.org/t/p/original/'
+    
+    let fetchData =async()=> {
 
         let result = await getData(path)
 
@@ -14,16 +17,28 @@ export default function Row({title,path,isLarge}) {
 
     useEffect(()=>{
 
-        let teste = fetchData()
-        teste.then ((data))=>{
-            
-        }
+        let consulta = fetchData()
+        consulta.then ((data)=>{
+            console.log(data?.results)
+            SetFilmes(data?.results)
+            })
 
     },[])
 
     return (
-        <div className='teste'> 
-        {title}
+        <div className='teste'>            
+            {title}
+
+            <div className='imagens'>
+                {filmes?.map( (filme) => {
+                    let cl = isLarge?'large':'normal'
+                    
+                    return <img className={cl} key={filme?.id} src={image_host + filme?.backdrop_path}></img>
+                })}
+                
+            </div>
+
+
         </div>
     )
 }
